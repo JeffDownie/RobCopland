a101cf97d38e9b3aead54e82711897b1df2db606
Rob Hern <robertjhern@gmail.com>
2017-05-25 18:29:24 +0100

    adding resources

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Shorthand for $( document ).ready()
+$(function() {
+    console.log( "ready!" );
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+});//end check ready
